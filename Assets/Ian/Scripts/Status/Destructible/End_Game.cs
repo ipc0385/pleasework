50447c3817c2e081c8faccb9ec67adaac1f16388
ipc0385 <ipc0385@louisiana.edu>
2016-03-20 17:00:01 -0500

    created game over script

---
+++ Assets/Ian/Scripts/Status/Destructible/End_Game.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class End_Game : Destructible
+{
+	public override void Destruct()
+	{
+		Application.LoadLevel(0);
+	}
+
+	public override bool isDestructed
+	{
+		get
+		{
+			//if the object were alive to call this function, it wouldn't be able to return true
+			return false;
+		}
+	}
+}
