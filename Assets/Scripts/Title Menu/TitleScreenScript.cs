8e28a494fd79ac80b0e80f034b469f578dbb7e38
spartan6261 <spartan6261@gmail.com>
2016-03-20 15:47:31 -0500

    Maze Stuff
    Incomplete

---
+++ Assets/Scripts/Title Menu/TitleScreenScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenScript : MonoBehaviour
+{
+    public Canvas quitMenu;
+    public Button playButton;
+    public Button quitButton;
+
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        playButton = playButton.GetComponent<Button>();
+        quitButton = quitButton.GetComponent<Button>();
+        quitMenu.enabled = false;
+    }
+
+    public void PlayClick() 
+    {
+        SceneManager.LoadScene(1); // load level 1       
+    }
+
+    public void QuitClick()
+    {       
+        quitMenu.enabled = true; 
+        var color = playButton.colors;
+        color.normalColor = Color.clear;
+        playButton.enabled = false; 
+        playButton.colors = color;
+        quitButton.enabled = false;
+        quitButton.colors = color;
+    }
+
+    public void NoClick() 
+    {
+        quitMenu.enabled = false; 
+        var color = playButton.colors;
+        color.normalColor = Color.black;
+        playButton.enabled = true; 
+        playButton.colors = color;
+        quitButton.enabled = true;
+        quitButton.colors = color;
+    }
+    
+    public void ExitGame() 
+    {
+        Application.Quit(); // quit the game 
+    }
+}
---
+++ Assets/Scripts/Title Menu/TitleScreenShip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleScreenShip : MonoBehaviour
+{
+
+    public float speed = (float)30;
+    public Vector3 pivot = new Vector3(50, 0, 50);
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        transform.RotateAround(pivot, -Vector3.one, speed * Time.deltaTime);
+    }
+}
